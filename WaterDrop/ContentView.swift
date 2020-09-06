9bc90d385d46b55302500377bf6855d0d0afeaf1
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-09-06 17:45:57 +0300

    Initial Commit

---
+++ WaterDrop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WaterDrop
+//
+//  Created by Ramill Ibragimov on 06.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WaterDrop/WaterDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterDropApp.swift
+//  WaterDrop
+//
+//  Created by Ramill Ibragimov on 06.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct WaterDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
