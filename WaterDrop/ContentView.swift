afc348e203bfa2478c7f1ac2719ff06013045e06
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-09-06 17:59:36 +0300

    init

--- WaterDrop/ContentView.swift
+++ WaterDrop/ContentView.swift
@@ -9,8 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: {
+            
+        }, label: {
+            ZStack {
+                WaterDropShape()
+                    .fill(LinearGradient(gradient: Gradient(colors: [.white, .blue]), startPoint: .topLeading, endPoint: .bottom))
+                    .frame(width: 110, height: 60)
+                    .shadow(color: .black, radius: 3, x: 1, y: 2)
+                Text("100ml")
+                    .foregroundColor(.white)
+                    .offset(y: 12)
+            }
+        })
+        
     }
 }
 
@@ -19,3 +31,15 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct WaterDropShape: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.size.width/2, y: 0))
+            path.addQuadCurve(to: CGPoint(x: rect.size.width/2, y: rect.size.height), control: CGPoint(x: rect.size.width, y: rect.size.height))
+            path.addQuadCurve(to: CGPoint(x: rect.size.width/2, y: 0), control: CGPoint(x: 0, y: rect.size.height))
+        }
+    }
+}
+
